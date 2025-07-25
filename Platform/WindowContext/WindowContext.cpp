392c79f39666d563d22311f0c2dea30124d54b14
Ext Culith <unirzine@gmail.com>
2025-07-25 17:39:11 +0800

    Refactor WindowContext to integrate BGFX for enhanced graphics rendering

--- Platform/WindowContext/WindowContext.cpp
+++ Platform/WindowContext/WindowContext.cpp
@@ -1,34 +1,165 @@
 #include "WindowContext.h"
+#include "Core/Logger/Logger.h"
+#include "Platform/PlatformException/PlatformException.h"
+#include <bgfx/platform.h>
 
-using namespace cyanvne;
+#if defined(__linux__) || defined(__unix__)
+    #define BX_PLATFORM_LINUX 1
+#elif defined(_WIN32) || defined(_WIN64)
+    #define BX_PLATFORM_WINDOWS 1
+#elif defined(__APPLE__)
+    #include "TargetConditionals.h"
+    #if TARGET_OS_MAC && !TARGET_OS_IPHONE
+        #define BX_PLATFORM_OSX 1
+    #endif
+#elif defined(__ANDROID__)
+    #define BX_PLATFORM_ANDROID 1
+#elif defined(__EMSCRIPTEN__)
+    #define BX_PLATFORM_EMSCRIPTEN 1
+#endif
 
-platform::WindowContext::WindowContext(const char* title, uint32_t width, uint32_t height, const char* drive, SDL_WindowFlags flags)
+using namespace cyanvne::platform;
+
+WindowContext::WindowContext(const char* title, uint32_t width, uint32_t height, uint32_t target_fps)
+    : window_{nullptr},
+      width_{width},
+      height_{height},
+      target_frame_duration_{0},
+      frame_buffer_handle_{BGFX_INVALID_HANDLE}
 {
-	// window part
-	core::GlobalLogger::getCoreLogger()->info("Try to create a window context");
-	window_ = SDL_CreateWindow(title, static_cast<int>(width), static_cast<int>(height),
-		flags | SDL_WINDOW_HIDDEN);
-	if (window_ == nullptr)
-	{
-		core::GlobalLogger::getCoreLogger()->critical("Failed to create window context : {:s}", SDL_GetError());
-		throw exception::platformexception::CreateWindowContextException("Failed to create window context");
-	}
+    if (target_fps > 0)
+    {
+        target_frame_duration_ = std::chrono::duration<double>(1.0 / target_fps);
+    }
 
-	// renderer part
-	drive_num_ = SDL_GetNumRenderDrivers();
-	core::GlobalLogger::getCoreLogger()->info("Supported Graphic API List: ");
-	render_drive_str_ = std::views::iota(0, drive_num_) |
-		std::views::transform([](int i) { return std::string(SDL_GetRenderDriver(i)); }) |
-		std::views::transform([](const std::string& str)
-			{ core::GlobalLogger::getCoreLogger()->info(str); return str; }) |
-		std::ranges::to<std::vector<std::string>>();	
-	core::GlobalLogger::getCoreLogger()->info("END");
+    window_ = SDL_CreateWindow(title, static_cast<int>(width), static_cast<int>(height), SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN);
+    if (!window_)
+    {
+        cyanvne::core::GlobalLogger::getCoreLogger()->critical("Failed to create window: {}", SDL_GetError());
+        throw cyanvne::exception::platformexception::CreateWindowContextException("Failed to create window");
+    }
 
-	sdl_renderer_ = SDL_CreateRenderer(window_, drive);
-	if (sdl_renderer_ == nullptr)
-	{
-		core::GlobalLogger::getCoreLogger()->critical("Failed to create graphic context : {:s}", SDL_GetError());
-		throw exception::platformexception::CreateWindowContextException("Failed to create graphic context");
-	}
-	SDL_ShowWindow(window_);
+    void* native_handle = getNativeWindowHandle();
+    if (native_handle == nullptr)
+    {
+        cyanvne::core::GlobalLogger::getCoreLogger()->critical("Failed to get native window handle for window: {}", title);
+        throw cyanvne::exception::platformexception::CreateWindowContextException("Failed to get native window handle");
+    }
+
+    frame_buffer_handle_ = bgfx::createFrameBuffer(native_handle, width_, height_);
+    if (!bgfx::isValid(frame_buffer_handle_))
+    {
+        cyanvne::core::GlobalLogger::getCoreLogger()->critical("Failed to create frame buffer for window: {}", title);
+        throw cyanvne::exception::platformexception::CreateWindowContextException("Failed to create frame buffer");
+    }
+
+    showWindow();
+}
+
+WindowContext::~WindowContext()
+{
+    if (bgfx::isValid(frame_buffer_handle_))
+    {
+        bgfx::destroy(frame_buffer_handle_);
+    }
+
+    if (window_)
+    {
+        SDL_DestroyWindow(window_);
+    }
+}
+
+void* WindowContext::getNativeWindowHandle()
+{
+    SDL_PropertiesID props = SDL_GetWindowProperties(window_);
+#if BX_PLATFORM_LINUX
+    return (void*)SDL_GetNumberProperty(props, "SDL.window.x11.window", 0);
+#elif BX_PLATFORM_WINDOWS
+    return SDL_GetPointerProperty(props, "SDL.window.win32.window", nullptr);
+#elif BX_PLATFORM_OSX
+    return SDL_GetPointerProperty(props, "SDL.window.cocoa.window", nullptr);
+#elif BX_PLATFORM_ANDROID
+    return SDL_GetPointerProperty(props, "SDL.window.android.window", nullptr);
+#elif BX_PLATFORM_EMSCRIPTEN
+    return SDL_GetPointerProperty(props, "SDL.window.html5.canvas", nullptr);
+#else
+    return nullptr;
+#endif
+}
+
+void WindowContext::reset(uint32_t width, uint32_t height)
+{
+    width_ = width;
+    height_ = height;
+
+    if (bgfx::isValid(frame_buffer_handle_))
+    {
+        bgfx::destroy(frame_buffer_handle_);
+    }
+
+    void* native_handle = getNativeWindowHandle();
+    if (native_handle == nullptr)
+    {
+       cyanvne::core::GlobalLogger::getCoreLogger()->error("Failed to get native handle on reset");
+       return;
+    }
+
+    frame_buffer_handle_ = bgfx::createFrameBuffer(native_handle, width_, height_);
+}
+
+SDL_Window* WindowContext::getWindowHandle() const
+{
+    return window_;
+}
+
+bgfx::FrameBufferHandle WindowContext::getFrameBufferHandle() const
+{
+    return frame_buffer_handle_;
+}
+
+std::chrono::duration<double> WindowContext::getTargetFrameDuration() const
+{
+    return target_frame_duration_;
+}
+
+void WindowContext::getWindowSize(int32_t* width, int32_t* height) const
+{
+    SDL_GetWindowSize(window_, width, height);
+}
+
+void WindowContext::showWindow()
+{
+    SDL_ShowWindow(window_);
+}
+
+void WindowContext::hideWindow()
+{
+    SDL_HideWindow(window_);
+}
+
+void WindowContext::setWindowPosition(int32_t x, int32_t y)
+{
+    SDL_SetWindowPosition(window_, x, y);
+}
+
+void WindowContext::setWindowSize(int32_t width, int32_t height)
+{
+    SDL_SetWindowSize(window_, width, height);
+
+    reset(static_cast<uint32_t>(width), static_cast<uint32_t>(height));
+}
+
+void WindowContext::setWindowFullscreen(bool fullscreen)
+{
+    SDL_SetWindowFullscreen(window_, fullscreen);
+}
+
+bool WindowContext::isMinimized() const
+{
+    return (SDL_GetWindowFlags(window_) & SDL_WINDOW_MINIMIZED);
+}
+
+bool WindowContext::isOccluded() const
+{
+    return (SDL_GetWindowFlags(window_) & SDL_WINDOW_OCCLUDED);
 }
--- Platform/WindowContext/WindowContext.h
+++ Platform/WindowContext/WindowContext.h
@@ -1,10 +1,9 @@
 #pragma once
+
 #include <SDL3/SDL.h>
-#include <Platform/PlatformException/PlatformException.h>
-#include <Core/Logger/Logger.h>
-#include <string>
-#include <vector>
-#include <ranges>
+#include <cstdint>
+#include <chrono>
+#include <bgfx/bgfx.h>
 
 namespace cyanvne
 {
@@ -13,149 +12,41 @@ namespace cyanvne
 		class WindowContext
 		{
 		public:
-			WindowContext(const char* title, uint32_t width = 1280, uint32_t height = 720, const char* drive = nullptr, SDL_WindowFlags flags = SDL_WINDOW_HIDDEN);
-			
+			WindowContext(
+				const char* title,
+				uint32_t width = 1280,
+				uint32_t height = 720,
+				uint32_t target_fps = 0
+			);
+			~WindowContext();
+
 			WindowContext(const WindowContext&) = delete;
-            WindowContext& operator=(const WindowContext&) = delete;
-			WindowContext(WindowContext&& other) noexcept
-			{
-				render_drive_str_ = std::move(other.render_drive_str_);
-                window_ = other.window_;
-                sdl_renderer_ = other.sdl_renderer_;
-				drive_num_ = other.drive_num_;
+			WindowContext& operator=(const WindowContext&) = delete;
+			WindowContext(WindowContext&&) = delete;
+			WindowContext& operator=(WindowContext&&) = delete;
 
-				other.window_ = nullptr;
-                other.sdl_renderer_ = nullptr;
-			}
-			WindowContext& operator=(WindowContext&& other) = delete;
-			
-			~WindowContext()
-			{
-				if (sdl_renderer_)
-				{
-					SDL_DestroyRenderer(sdl_renderer_);
-				}
-				if (window_)
-				{
-					SDL_DestroyWindow(window_);
-				}
-			}
-			bool showWindow() const
-			{
-				if (!SDL_ShowWindow(window_))
-					return false;
-				return true;
-			}
-			bool hideWindow() const
-				// maybe thread safe
-			{
-				if (!SDL_HideWindow(window_))
-					return false;
-				return true;
-			}
-			bool presentRender() const
-			{
-				if (!SDL_RenderPresent(sdl_renderer_))
-				{
-					SDL_LogError(SDL_LOG_CATEGORY_RENDER, "Can not render");
-					return false;
-				}
-				return true;
-			}
-			SDL_Renderer* getRendererHinding() const
-			{
-				return sdl_renderer_;
-			}
-			SDL_Window* getWindowHinding() const
-			{
-				return window_;
-			}
-			[[nodiscard]] bool setRenderVSync(int32_t vsync) const
-			{
-				if (!SDL_SetRenderVSync(sdl_renderer_, vsync))
-				{
-					SDL_LogError(SDL_LOG_CATEGORY_RENDER, "Fail to set render vsync");
-					return false;
-				}
-				return true;
-			}
-			void setWindowPosition(int32_t x = SDL_WINDOWPOS_CENTERED, int32_t y = SDL_WINDOWPOS_CENTERED) const
-			{
-				SDL_SetWindowPosition(window_, x, y);
-			}
-			bool isMinimized() const
-			{
-				return (SDL_GetWindowFlags(window_) & SDL_WINDOW_MINIMIZED);
-			}
-			[[nodiscard]] bool setRenderDrawColorInt(uint8_t r, uint8_t g, uint8_t b, uint8_t a) const
-			{
-				if (SDL_SetRenderDrawColor(sdl_renderer_, r, g, b, a))
-					return true;
-				return false;
-			}
+			void reset(uint32_t width, uint32_t height);
 
-			[[nodiscard]] bool setRenderDrawColorFloat(float r, float g, float b, float a) const
-			{
-				if (SDL_SetRenderDrawColorFloat(sdl_renderer_, r, g, b, a))
-					return true;
-				return false;
-			}
-			void renderClear() const
-			{
-				SDL_RenderClear(sdl_renderer_);
-			}
-			SDL_Rect getWindowRect() const
-				// not include point, only have w,h
-			{
-				SDL_Rect tmp_rect{ 0, 0, 0, 0 };
-				SDL_GetWindowSize(window_, &(tmp_rect.w), &(tmp_rect.h));
-				return tmp_rect;
-			}
-			SDL_Surface* getWindowSurfaceCopy() const
-			{
-				SDL_Surface* current_surface = SDL_GetWindowSurface(window_);
-				SDL_Surface* copy = SDL_CreateSurface(current_surface->w, current_surface->h, current_surface->format);
-				SDL_BlitSurface(current_surface, nullptr, copy, nullptr);
-				return copy;
-			}
-			SDL_Texture* surfaceToTexture(SDL_Surface* src) const
-			{
-				return SDL_CreateTextureFromSurface(sdl_renderer_, src);
-			}
-			void renderTexture(SDL_Texture* src, const SDL_FRect* src_rect = nullptr, const SDL_FRect* dst_rect = nullptr) const
-			{
-				SDL_RenderTexture(sdl_renderer_, src, src_rect, dst_rect);
-			}
-			void renderLine(float x1, float y1, float x2, float y2) const
-			{
-				SDL_RenderLine(sdl_renderer_, x1, y1, x2, y2);
-			}
-			void setWindowFullScreen(bool fullscreen) const
-			{
-				if (fullscreen)
-				{
-					SDL_SetWindowFullscreen(window_, true);
-				}
-				else
-				{
-					SDL_SetWindowFullscreen(window_, false);
-				}
-			}
-			void setWindowSize(int32_t width, int32_t height) const
-			{
-				SDL_SetWindowSize(window_, width, height);
-			}
-			void syncWindowChange() const
-			{
-				SDL_SyncWindow(window_);
-			}
+			SDL_Window* getWindowHandle() const;
+			bgfx::FrameBufferHandle getFrameBufferHandle() const;
+			std::chrono::duration<double> getTargetFrameDuration() const;
+			void getWindowSize(int32_t* width, int32_t* height) const;
+
+			void showWindow();
+			void hideWindow();
+			void setWindowPosition(int32_t x, int32_t y);
+			void setWindowSize(int32_t width, int32_t height);
+			void setWindowFullscreen(bool fullscreen);
+			bool isMinimized() const;
+			bool isOccluded() const;
 		private:
-			// Surface-based software rendering is no longer used by default.
-			// Software rendering cannot be used with hardware rendering
-			std::vector<std::string> render_drive_str_;
-			int32_t drive_num_;
-			SDL_Renderer* sdl_renderer_;
+			void* getNativeWindowHandle();
+
 			SDL_Window* window_;
+			uint32_t width_;
+			uint32_t height_;
+			std::chrono::duration<double> target_frame_duration_;
+			bgfx::FrameBufferHandle frame_buffer_handle_;
 		};
 	}
 }
