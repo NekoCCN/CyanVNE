4b403316529a6992f9fe6c033eb146c4dbc90c0b
Ext Culith <unirzine@gmail.com>
2025-08-14 16:27:19 +0800

    Add bgfx integration for ImGui with embedded shaders and rendering functions.

---
+++ Platform/GuiContext/Detail/imgui_impl_bgfx.h
@@ -0,0 +1,14 @@
+//
+// Created by unirz on 2025/8/14.
+//
+
+#ifndef CYANVNE_IMGUI_IMPL_BGFX_H
+#define CYANVNE_IMGUI_IMPL_BGFX_H
+
+
+class imgui_impl_bgfx {
+
+};
+
+
+#endif //CYANVNE_IMGUI_IMPL_BGFX_H
