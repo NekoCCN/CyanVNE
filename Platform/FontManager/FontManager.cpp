a5d86753d54f015ef0aad0c0564691b093cf1313
Ext Culith <unirzine@gmail.com>
2025-08-20 22:52:16 +0800

    Add FontManager class and header file for font management.

---
+++ Platform/FontManager/FontManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by unirz on 2025/8/20.
+//
+
+#include "FontManager.h"
---
+++ Platform/FontManager/FontManager.h
@@ -0,0 +1,17 @@
+//
+// Created by Ext Culith on 2025/8/20.
+//
+
+#ifndef CYANVNE_FONTMANAGER_H
+#define CYANVNE_FONTMANAGER_H
+
+
+namespace cyanvne::platform
+{
+    class FontManager
+    {
+
+    };
+}
+
+#endif //CYANVNE_FONTMANAGER_H
