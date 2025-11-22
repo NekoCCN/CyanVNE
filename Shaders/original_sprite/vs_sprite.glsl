a0a49de693446836bd806ebfdf9835c5fd6af5fb
Ext Culith <unirzine@gmail.com>
2025-11-22 21:27:07 +0800

    Simplify vertex position calculation in vs_sprite.glsl

--- Shaders/original_sprite/vs_sprite.glsl
+++ Shaders/original_sprite/vs_sprite.glsl
@@ -5,8 +5,7 @@ $output v_texcoord0, v_color0
 
 void main()
 {
-    gl_Position = mul(u_proj, mul(u_view, mul(u_model[0], vec4(a_position.xy, 0.0, 1.0))));
-
+    gl_Position = mul(u_proj, mul(u_view, mul(u_model[0], vec4(a_position, 1.0))));
     v_texcoord0 = a_texcoord0;
     v_color0 = a_color0;
 }
